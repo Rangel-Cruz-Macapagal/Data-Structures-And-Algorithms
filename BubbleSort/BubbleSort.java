baefd41fcdfe77730c999dab52a58eaddb85a2a4
Rangel-Cruz-Macapagal <angelomacapagal52@gmail.com>
2016-10-05 13:18:57 +0800

    Add files via upload

---
+++ BubbleSort/BubbleSort.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Random;
+public class BubbleSort {
+	public static void main(String [] args){
+		Scanner in=new Scanner(System.in);
+		Random rand= new Random();
+		int num, swap=0;
+		int [] array;
+		
+		System.out.print("Enter the number of arrays you want to sort: ");
+		num=in.nextInt();
+		array=new int[num];
+		
+		for(int i=0; i<num; i++){
+		System.out.println(array[i]=rand.nextInt(100));
+			for(int j=0; j<i; j++){
+				if(array[j]<array[j+1]){
+					swap=array[j];
+					array[j]=array[j+1];
+					array[j+1]=swap;
+				}
+			}
+		}
+		
+	}
+}
