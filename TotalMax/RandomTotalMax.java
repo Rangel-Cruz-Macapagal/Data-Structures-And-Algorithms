47568696bd7fcd9b29059e6c35c1ea10e7d21aef
Rangel-Cruz-Macapagal <angelomacapagal52@gmail.com>
2016-10-05 13:15:49 +0800

    Add files via upload

---
+++ TotalMax/RandomTotalMax.java
@@ -0,0 +1,53 @@
+/*Rangel Angelo C. Macapagal
+  ACT-2
+  DSA
+ */
+
+import java.util.*;
+import java.util.Random;
+public class RandomTotalMax {
+ public static void main(String[]args){
+	 Scanner in=new Scanner(System.in);
+	 Random t= new Random();
+		int  size=0;
+		int array[];
+		int sum=0;
+
+		int max=Integer.MIN_VALUE;
+		int min=Integer.MAX_VALUE;
+		
+		
+		System.out.print("Enter the number that you want:");
+		size=in.nextInt();
+		array =new int[size];
+		System.out.println("Random "+size+" numbers:");
+		
+		
+		
+		for(int i=0;i<array.length;i++){
+			System.out.println(array[i]=t.nextInt(100));
+		}
+		sum=0;
+		for(int i=0;i<array.length;i++){
+			sum = sum +array[i];
+		    }
+		    double ave =(double) sum /array.length;
+		    System.out.println("Sum ="+sum);
+		    System.out.println("Average ="+ave);
+		    max =array[0];
+		    for (int i=0;i<array.length;i++){
+		        if(min>array[i]){
+					 min= array[i];
+				 }
+		    	if(array[i]>max){
+					 max= array[i];
+				 }
+		    }
+		    System.out.println("Max ="+max);
+		    System.out.println("Min ="+min);
+		    
+		
+ }
+}
+
+
